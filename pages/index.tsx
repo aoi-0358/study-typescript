5c9a5c5cf100372821beb79de9abdf27d4d9594e
aoi-0358 <aogangcun@gmail.com>
2023-02-25 08:28:19 +0900

    #7オブジェクトの型注釈について学習した

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,15 @@
 import type { NextPage } from "next";
 
+let obj3: Record<string, unknown> = {
+  a: 1,
+  b: "foo",
+};
+let obj4: { [key: string]: { foo: unknown } } = {
+  a: {
+    foo: 1,
+  },
+};
+
 const Home: NextPage = () => {
   return <div>test</div>;
 };
