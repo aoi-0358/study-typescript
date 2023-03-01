7f6e8388b5f8a39a82bd261d82370706ecbeadb6
aoi-0358 <aogangcun@gmail.com>
2023-03-02 07:10:32 +0900

    ✨MappedTypesの応用について

--- index.ts
+++ index.ts
@@ -1,20 +1,16 @@
 export type User = {
   name: string;
 } & PersonalData;
-// インターセクションタイプでPersonalDataをくっつけている
-
-type Foo = "height" | "weigth";
 
 type PersonalData = {
-  // heigtht: number;
-  // weight: number;
-  [k in "height" | "weight"]: number;
-  // kはなんでもいいが慣例的にkeyのk、propertiesnのpが多い
-  //　[k in "height" | "weight"]: number;でオブジェクトのプロパティ名をマッピング（配置）
-  //　↑MappedTypesのメリット
-  //  1."height" | "weight"と書かなくても上で定義してるFooを代入しても同じ意味
-  //  2.個人的にはリファクタリングに便利かも🤔
+  height: number;
+  weight: number;
+  realname: string;
 };
+type OptionalPersonalData ={
+  [k in keyof PersonalData ]: PersonalData[k];
+  //↑のMappedTypesでPersonalDataに後で追加されたプロパティrealname: string;をOptionalPersonalDataに自動で追加できる
+}
 
 const user: User = {
   name: "iii",
