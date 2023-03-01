f127877966e3863e72ba9f956718803c0274ec95
aoi-0358 <aogangcun@gmail.com>
2023-03-02 06:33:47 +0900

    ✨IndexSignatureについて学習と解説

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 export type User = {
   name: string;
-  [key: string]: string;
-  //[]で囲われてるとこがindex Signature
+  age: number;
+  [key: string]: string | number | undefined;
+  // [key: string]: string;
+  // []で囲われてるとこがindex Signatureで動的にプロパティを入れたい時に使う、UnionTypesで定義してない型を下のUserに入れるとエラーになるのが欠点
+  // 型がもろくなりがちなので、型がわかるのであれば素直に（上　name: string;　下  name: "iii",と書いたほうがいいのでindex Signatureは多様すべきではない
 };
 
 const user: User = {
   name: "iii",
+  age: 20,
   account: "shiam.it",
+  time: "seven_oclock",
 };
+
+user.account;
