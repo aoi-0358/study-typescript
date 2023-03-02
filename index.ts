25253c31a15f5ac15a84cb99562c9133912934ec
aoi-0358 <aogangcun@gmail.com>
2023-03-02 14:59:15 +0900

    ✨ ユーザー定義の型ガードで外部APIのレスポンスに型をつけた

--- index.ts
+++ index.ts
@@ -1,16 +1,23 @@
 type UserA = { name: string; lang: "ja" };
 type UserB = { name: string; lang: "en" };
 
-
-export const foo = (value: UserA | UserB) => {
-  if (value.lang === "ja") {
-    return value;
-  }
-
-  return value;
+const isUserA = (user: UserA | UserB): user is UserA => {
+  return user.lang === "ja";
 };
 
-// タグ付きUnion Types頻出する
-// (value.lang === "ja")が"ja"であれば１個目のreturn value;がUserAで早期returnしてるから下のreturn value;でUserB
-// 
+const isUserB = (user: UserA | UserB): user is UserB => {
+  return user.lang === "en";
+};
 
+export const foo = async () => {
+  const res = await fetch("");
+  const json = await res.json();
+  if (isUserA(json)) {
+    return json.lang;
+  }
+};
+
+// ユーザー定義の型ガード　頻出
+// １５で取得した外部のAPIに１６で型をつけたいがjsonをホバーするとanyで扱いづらい
+// なので６〜８の関数user is UserAで型を絞り込んで１７のif文でjsonをホバーするとUserAの型が取得できてる
+//外部のAPIを叩いた時のレスポンスに型をつけたい時に良い
