45c1411ddf4548fd252ee15ed13443bdb5cd16f4
aoi-0358 <aogangcun@gmail.com>
2023-03-03 08:26:54 +0900

    ✨Lookup TypesとGenericsを組み合わせた

--- index.ts
+++ index.ts
@@ -1,55 +1,16 @@
-export const foo = <T extends string | number>(arg: T) => {
-  if (typeof arg === "string") {
-    return { value: arg.toUpperCase() };
-  }
-  return { value: arg.toFixed };
+export const getProperty = <T, K extends keyof T>(obj: T, key: K) => {
+  return obj[key];
 };
-
-//　関数のGenerics　extendsによる型制約　超重要！！！
-
-// export const foo = <T>(arg: T) => {
-// return { value: arg };
+// export const getProperty = <T, K>(obj: T, key: K) => {
+//   return obj[key];
 // };
-// このコードだとargが中でどんな型になってるのかかわからない
-// T自体がなに分からない、argは型制約がないとunknownになる
-// unknownになると中で何かしらのメゾットが呼び出せない問題がある
-// もし、(arg: T)にstringがくるとわかっていたらstringのメゾットが使える
+// オブジェクトを第１引数にわたして、そのオブジェクトのkeyを第二引数にわたしてそのvalueを返すコード
+// obj[key]でエラー（型 'K' はインデックスの種類 'T' に使用できません。）
+// 型があってないっと仮定
 
-// どうやってstringを定義するのか？
-// export const foo = <T extends string>(arg: T) => {
-//   arg.toUpperCase
-//   return { value: arg };
+// Lookup Typesで解決
+// export const getProperty = <T, K extends keyof T>(obj: T, key: K) => {
+//   return obj[key];
 // };
-// <T extends string>と書けば(arg: T)はstringを満たすと証明できstringがもつメゾットにアクセスできる
-// 実際の開発ではarg.toUpperCaseにargを使っていろんな記述をかくことになる
-// なのでargがunknownの状態だったら不都合が生じるのでextendsによる型制約は必須
-
-// UnionTypesで型を定義した場合
-// export const foo = <T extends string | number>(arg: T) => {
-//   arg.toUpperCase
-//   return { value: arg };
-// };
-//　↑はtoUpperCaseで（プロパティ 'toUpperCase' は型 'number' に存在しません）のエラーになる
-// なぜか？
-// (arg: T)とゆうのはnumberかstring両方の可能性かあることになる
-// なのでnumberだった場合toUpperCaseは存在しないからアクセスできなくてエラー
-
-// なのでif文を使って型を絞り込んで解決
-// if (typeof arg === "string") {
-//   arg.toUpperCase;
-// }
-// return { value: arg };
-// };
-// (typeof arg === "string")でargがstringの場合は
-// arg.toUpperCase;が使える
-
-// 以下のように記述すると５０でreturnしているのでstringのメゾットが使える
-// ５２はif文の後でnumberの制約を満たす型になるので、numberのメゾットtoFixedが使える
-// export const foo = <T extends string | number>(arg: T) => {
-//   if (typeof arg === "string") {
-//     return { value: arg.toUpperCase() };
-//   }
-//   return { value: arg.toFixed };
-// };
-
-
+// 第１引数に何かしらのオブジェクトを入れることになるが、keyofでTと一緒にしてる
+// K extendには必ずオブジェクトのkeyが入るようになってLookup Typesがはたらく
